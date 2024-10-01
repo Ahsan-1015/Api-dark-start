cfb125251d67fde52a8d5ff4311ccf4fc999ae9e
Ahsan-1015 <ahshanh964@gmail.com>
2024-10-01 21:04:19 +0600

    comment.js async api commited

---
+++ js/comments.js
@@ -0,0 +1,19 @@
+// normal way te
+const loadComments = () => {
+  fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('error happend ', error));
+};
+
+// async way te
+
+const loadComments2 = async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments');
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
